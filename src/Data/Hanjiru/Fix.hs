2ffebbc3356ba2ff8dba73ef701f10dcd3426bd7
exclusive-and <exclusiveandgate@gmail.com>
2025-01-07 09:29:36 -0500

    Elaborated docs in terms of convergence

--- src/Data/Hanjiru/Fix.hs
+++ src/Data/Hanjiru/Fix.hs
@@ -3,13 +3,17 @@ module Data.Hanjiru.Fix where
 import Control.Monad.Trans.Writer
 import Data.Monoid (Any (..))
 
+-- | Keep a qualitative record of whether something changed.
+--
+-- Change indirectly measures convergence: a function has converged when applying it again
+-- does not result in any change to its input.
 type Changes = Writer Any
 
 -- | Record that something changed.
 changed :: a -> Changes a
 changed x = tell (Any True) >> pure x
 
--- | Apply a function to a value if the value has a recorded change.
+-- | Apply a function to a value if it hasn't converged.
 apChanges :: (a -> a) -> Changes a -> a
 apChanges f cx =
     let
@@ -17,7 +21,7 @@ apChanges f cx =
     in
         if getAny hasChanges then f y else y
 
--- | Repeatedly apply a function to a value until it does not record any more changes.
+-- | Repeatedly apply a function to a value until it converges.
 saturate :: (a -> Changes a) -> a -> a
 saturate f = go
     where
