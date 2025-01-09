9c7c83ae6e75cfd93024243eef18494628a096b6
exclusive-and <exclusiveandgate@gmail.com>
2025-01-09 15:56:18 -0500

    Prototype of iterated first set calculation

--- src/Hanjiru/MakeParser/Lookahead.hs
+++ src/Hanjiru/MakeParser/Lookahead.hs
@@ -11,6 +11,24 @@ import Data.Set (Set)
 import Data.Set qualified as Set
 import Hanjiru.MakeParser.Rules
 
+-- | Calculate a set of prefix terminals for each non-terminal in the language.
+firstSets :: Ord a => Map a (NonEmpty (Rule a)) -> Map a (Set a)
+firstSets rules =
+    Map.map fst
+        $ saturate (\xs -> traverse (go xs) xs)
+        $ Map.map (Set.empty,) rules
+    where
+        go xs x@(old, alts)
+            | new == old = pure x
+            | otherwise  = changed (new, alts)
+            where
+                new = foldr (<>) Set.empty (goAlt xs <$> alts)
+
+        goAlt xs (Rule _ (x :| _)) =
+            case Map.lookup x xs of
+                Just (s, _) -> s
+                Nothing     -> Set.singleton x
+
 -- | Drill down into a symbol to get the set of all terminals that can prefix it.
 first :: Ord a => View a -> Set a
 first x = evalState (go x) Set.empty
