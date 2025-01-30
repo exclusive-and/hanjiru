31ead44575b490e2129f552a2f2331421fc344b9
exclusive-and <exclusiveandgate@gmail.com>
2025-01-30 14:02:40 -0500

    Prototype LR(0) table construction

--- src/Hanjiru/MakeParser/LR0.hs
+++ src/Hanjiru/MakeParser/LR0.hs
@@ -1,7 +1,11 @@
 module Hanjiru.MakeParser.LR0 where
 
-import Data.Hanjiru.Strategies
+import Control.Arrow
+import Control.Monad.Trans.State
+import Data.Foldable (foldrM)
+import Data.Hanjiru.MapGraph
 import Data.List qualified as List
+import Data.List.NonEmpty (NonEmpty)
 import Data.List.NonEmpty qualified as NE
 import Data.Map (Map)
 import Data.Map qualified as Map
@@ -38,3 +42,35 @@ successors (LR items) =
             [ (x, LR (Set.singleton $ Item tok alt xs))
             | Item tok alt (x:xs) <- Set.toList items
             ]
+
+data Action a   = Shift  a Int
+                | Reduce (Rule a)
+
+type ActionMap a = Map Int [Action a]
+
+type GotoMap a = Map (Int, a) Int
+
+makeLr0 :: Ord a => Map a (NonEmpty (Rule a)) -> Rule a -> (ActionMap a, GotoMap a)
+makeLr0 grammar goal =
+    let
+        MapGraph edges nodes = unfold (Map.toList . successors) initial
+    in
+        first (Map.unionWith (<>) (Map.map reductions nodes)) $ collectShiftsAndGotos edges
+    where
+        initial =
+            let
+                Rule tok xs = goal
+                xs' = view grammar <$> xs
+            in
+                closure $ LR $ Set.singleton $ toItem tok (Alt xs') xs'
+
+reductions :: LR a -> [Action a]
+reductions (LR items) = [ Reduce (toRule item) | item@(Item _ _ []) <- Set.toList items ]
+
+collectShiftsAndGotos :: Ord a => Map (Int, View a) Int -> (ActionMap a, GotoMap a)
+collectShiftsAndGotos = Map.foldrWithKey go (mempty, mempty)
+    where
+        go (v, e) w (shifts, gotos) =
+            case e of
+                NonTerm tok _   -> (shifts, Map.insert (v, tok) w gotos)
+                Term tok        -> (Map.insert v [Shift tok w] shifts, gotos)
