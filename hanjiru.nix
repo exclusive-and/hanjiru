c6a7b0dab61f8765dbd53116ab98da8a65cadb2a
xand <exclusiveandgate@gmail.com>
2025-02-27 19:50:14 -0500

    removed unnecessary let-in expression

--- hanjiru.nix
+++ hanjiru.nix
@@ -8,21 +8,18 @@
 , transformers
 }:
 
-let
-    hanjiru = mkDerivation {
-        pname = "hanjiru";
-        version = "0.1.0.0";
-        src = ./.;
-        libraryHaskellDepends = [
-            base
-            containers
-            mtl
-            prettyprinter
-            primitive
-            transformers
-        ];
-        librarySystemDepends = [];
-        license = lib.licenses.bsd3;
-    };
-in
-    hanjiru
+mkDerivation {
+    pname = "hanjiru";
+    version = "0.1.0.0";
+    src = ./.;
+    libraryHaskellDepends = [
+        base
+        containers
+        mtl
+        prettyprinter
+        primitive
+        transformers
+    ];
+    librarySystemDepends = [];
+    license = lib.licenses.bsd3;
+}
